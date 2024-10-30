208765ba5f3d4814079eba78211c1115325f3bea
FloraIsDesignWebs <flora.jiang1990@gmail.com>
2024-10-30 18:48:13 -0400

    Adding week 2 and 3 assingment

---
+++ week2/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html lang='en'>
+<head>
+    <meta charset="utf-8">
+    <title>HTTP5225 Class</title>
+</head>
+<body>
+    <h1>Learning PHP - Week 2</h1>
+    <?php
+    echo "<h2>Hello World!</h2>"
+    ?>
+    <?php
+    $fname="Gary";
+    // connect two string, not use +, we use "."
+    echo "<h2>welcome</h2>" . $fname;
+
+    echo "<h2>welcome " . $fname. "</h2>";
+    ?>
+
+<?php
+$people = array(
+  'Jane', 
+  'Bill', 
+  'Harminder');
+
+$person['first'] = 'John';
+$person['last'] = 'Alexander';
+?>
+
+
+</body>
+
+
+
